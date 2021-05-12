ceb36327d49f1c91c6baa341c3e6ed0fa87d2443
areum <whitestar503@naver.com>
2021-05-12 22:43:37 +0900

    3장 posts entity 클래스

---
+++ src/main/java/com/mome/hauoli/domain/posts/PostsRepository.java
@@ -0,0 +1,5 @@
+package com.mome.hauoli.domain.posts;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts,Long>{
+}
---
+++ src/test/java/com/mome/hauoli/web/domain/posts/PostsRepositoryTest.java
@@ -0,0 +1,14 @@
+package com.mome.hauoli.web.domain.posts;
+
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+public class PostsRepositoryTest {
+}
