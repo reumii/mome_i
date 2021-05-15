ed68497951eada469ebbaa3ad7f3add634466229
areum <whitestar503@naver.com>
2021-05-15 22:45:12 +0900

    3.4 등록/수정/조회 API 만들기

---
+++ src/main/java/com/mome/hauoli/service/posts/PostsService.java
@@ -0,0 +1,19 @@
+package com.mome.hauoli.service.posts;
+
+import com.mome.hauoli.domain.posts.PostsRepository;
+import com.mome.hauoli.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+
+    }
+}
--- src/test/java/com/mome/hauoli/web/domain/posts/PostsRepositoryTest.java
+++ src/test/java/com/mome/hauoli/web/domain/posts/PostsRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.mome.hauoli.web.domain.posts;
 
+import com.mome.hauoli.domain.posts.Posts;
+import com.mome.hauoli.domain.posts.PostsRepository;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -10,5 +12,37 @@ import org.springframework.test.context.junit4.SpringRunner;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class PostsRepositoryTest {
+
+    @Autowired
+    PostsRepository postsRepository;
+
+    @After
+    public void cleanup(){
+        postsRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기(){
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postsRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("jojoldu@gamil.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postsRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        assertThat(posts.getTitle()).isEqualTo(title);
+        assertThat(posts.getContent()).isEqualTo(content);
+    }
 }
