37763480701357bea314478754fb703c12a5fe8d
areum <whitestar503@naver.com>
2021-05-23 22:41:24 +0900

    3.4 등록/수정/조회 API 만들기

--- src/main/java/com/mome/hauoli/service/posts/PostsService.java
+++ src/main/java/com/mome/hauoli/service/posts/PostsService.java
@@ -1,7 +1,10 @@
 package com.mome.hauoli.service.posts;
 
+import com.mome.hauoli.domain.posts.Posts;
 import com.mome.hauoli.domain.posts.PostsRepository;
+import com.mome.hauoli.web.dto.PostsResponseDto;
 import com.mome.hauoli.web.dto.PostsSaveRequestDto;
+import com.mome.hauoli.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,5 +18,16 @@ public class PostsService {
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
 
+    }
+
+    @Transactional
+    public Long update(Long id, PostsUpdateRequestDto requestDto){
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        return id;
+    }
+
+    public PostsResponseDto findById(Long id){
+        Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        return new PostsResponseDto(entity);
     }
 }
