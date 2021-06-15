1220e3b8c791caf321ccf01bade504926e62e8a9
areum <whitestar503@naver.com>
2021-06-15 16:01:36 +0900

    ch4. 머스테치로 화면 구성하기

--- src/main/java/com/mome/hauoli/service/posts/PostsService.java
+++ src/main/java/com/mome/hauoli/service/posts/PostsService.java
@@ -2,12 +2,16 @@ package com.mome.hauoli.service.posts;
 
 import com.mome.hauoli.domain.posts.Posts;
 import com.mome.hauoli.domain.posts.PostsRepository;
+import com.mome.hauoli.web.dto.PostsListResponseDto;
 import com.mome.hauoli.web.dto.PostsResponseDto;
 import com.mome.hauoli.web.dto.PostsSaveRequestDto;
 import com.mome.hauoli.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -17,17 +21,29 @@ public class PostsService {
     @Transactional
     public Long save(PostsSaveRequestDto requestDto){
         return postsRepository.save(requestDto.toEntity()).getId();
-
     }
 
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto){
         Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        posts.update(requestDto.getTitle(), requestDto.getContent());
         return id;
     }
 
     public PostsResponseDto findById(Long id){
         Posts entity = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc(){
+        return postsRepository.findAllDesc().stream().map(PostsListResponseDto::new).collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id){
+        System.out.print("service");
+        Posts posts = postsRepository.findById(id).orElseThrow(()->new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        postsRepository.delete(posts);
     }
 }
